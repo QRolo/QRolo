2b803105f0d386a56726db11d904fcc4fda1b3e2
S <30815731+slimlime@users.noreply.github.com>
2021-02-18 19:16:39 +1000

    feat: create base plugin template
    Web platform, plugin template; through `flutter create --template plugin --platforms web .` Flutter
    web compatibility layer for QR scanner wrapping
    
    BREAKING CHANGE: Would potentially break people if anyone used this blank repo

---
+++ lib/qrolo.dart
@@ -0,0 +1,14 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class Qrolo {
+  static const MethodChannel _channel =
+      const MethodChannel('qrolo');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
---
+++ test/qrolo_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:qrolo/qrolo.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('qrolo');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await Qrolo.platformVersion, '42');
+  });
+}
