194f8e1c9e388b274917e37d0b99ab7411100b2f
S <30815731+slimlime@users.noreply.github.com>
2021-02-20 09:49:16 +1000

    chore(bit_matrix): add data data type clamped list

--- lib/src/dart_facade/bit_matrix.dart
+++ lib/src/dart_facade/bit_matrix.dart
@@ -1,5 +1,5 @@
 @JS()
-library BitMatrix;
+library bit_matrix;
 
 import 'dart:typed_data' show Uint8ClampedList;
 
@@ -7,17 +7,21 @@ import 'package:js/js.dart' show JS;
 
 @JS()
 class BitMatrix {
+  external factory BitMatrix(Uint8ClampedList data, num width);
+
   // @Ignore
   BitMatrix.fakeConstructor$();
+
   external static BitMatrix createEmpty(num width, num height);
   external num get width;
   external set width(num v);
   external num get height;
   external set height(num v);
-  external dynamic get data;
-  external set data(v);
-  external factory BitMatrix(Uint8ClampedList data, num width);
+  external Uint8ClampedList get data;
+  external set data(Uint8ClampedList v);
   external bool get(num x, num y);
+  // If this was a first-party lib we would be able to decide named bool.
+  // ignore: avoid_positional_boolean_parameters
   external void set(num x, num y, bool v);
   external void setRegion(num left, num top, num width, num height, bool v);
 }
