e8ae45166244b282032a2feda6c81e818df2e3d3
ml <30815731+slimlime@users.noreply.github.com>
2021-02-27 15:24:57 +1000

    refactor(media): wrap mediadevice infos for input devices pre-video check
    Dart html does not have a wrapper for InputDeviceInfo (camera microphone), only MediaDeviceInfo

--- lib/src/html/media/media_devices/is_media_device_camera_available.dart
+++ lib/src/html/media/media_devices/is_media_device_camera_available.dart
@@ -1,19 +1,24 @@
 import 'dart:html' as html show window, MediaDeviceInfo;
 
 import 'package:flutter/material.dart';
+import 'package:qrolo/src/html/media/media_devices/get_media_devices.dart';
 
 /// Check media devices within current window / iframe
+///
+/// Camera and microphone is actually `InputDeviceInfo`
 Future<bool> isCameraAvailableInMediaDevices() async {
-  final mediaDevicesGenerated =
-      await html.window.navigator.mediaDevices!.enumerateDevices();
+  final mediaDevicesGenerated = await getMediaDeviceInfosOrInputDeviceInfos();
 
   // InputDeviceInfo or MediaDeviceInfo
   final List<html.MediaDeviceInfo> mediaDeviceInfos =
       mediaDevicesGenerated.cast<html.MediaDeviceInfo>();
+  // Camera and microphone is actually `InputDeviceInfo`
+  final List<html.MediaDeviceInfo> cameras = await getVideoInputDevices();
 
   debugPrint(
-    'sources: ${mediaDeviceInfos.toString()}',
+    'media sources: ${mediaDeviceInfos.toString()}',
   );
+  debugPrint('video sources: $cameras');
 
   final bool isCameraFound = isVideoInputWithin(mediaDeviceInfos);
 
